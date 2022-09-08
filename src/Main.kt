5444de78f35016c04b11d109ac55e59c39d18eb4
Vinh Le <tun27687@temple.edu>
2022-09-08 09:40:39 -0400

    initial commit

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+
+fun main(args: Array<String>) {
+    val sq = Shape("Square");
+    val cir = Shape("Circle");
+    val tri = Shape("Triangle");
+    val equi = Shape("EquilateralTriangle");
+
+}
+
