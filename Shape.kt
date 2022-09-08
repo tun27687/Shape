ca8ab48fd86b10ed9a3d3ce9f210fb1f7948af8e
Vinh Le <77693627+tun27687@users.noreply.github.com>
2022-09-07 21:23:21 -0400

    Create Shape.kt
    Shape.kt

---
+++ Shape.kt
@@ -0,0 +1,149 @@
+
+open class Shape {
+
+    constructor (_name: String) {
+
+        when (_name) {
+            "square" -> {
+                val sq = Square(_name = "")
+                sq.setDimensions()
+                sq.printDimensions()
+                sq.getArea()
+            }
+            "circle" -> {
+                val cir = Circle(_name = "")
+                cir.setDimensions()
+                cir.printDimensions()
+                cir.getArea()
+            }
+            "triangle" -> {
+                val tri = Triangle(_name = "")
+                tri.setDimensions()
+                tri.printDimensions()
+                tri.getArea()
+            }
+
+            "equilateralTriangle" -> {
+                val equi = EquilateralTriangle(_name = "")
+                equi.setDimensions()
+                equi.printDimensions()
+                equi.getArea()
+            }
+
+        }
+
+
+    }
+
+
+    open fun getArea() {
+        println("Area is 0.0")
+    }
+
+    open fun printDimensions() {
+        println("No dimensions");
+    }
+
+    open fun setDimensions() {
+        println("No dimensions");
+    }
+
+    //Square class inherited from Shape
+    open class Square(_name: String) : Shape(_name) {
+        private var length = 0.0;
+
+        // getArea, setDimensions and printDimensions methods are Shape class methods. To
+        //override them in child classes
+        override fun getArea() {
+            println("Area is : " + length * length)
+        }
+
+        // overriding setDimensions() method
+        override fun setDimensions() {
+            println("Enter Side Length of Square : ")
+            length = readLine()?.toDouble() ?: 0.0
+        }
+
+        // overriding printDimensions() method
+        override fun printDimensions() {
+            println("Side Length is : $length")
+        }
+    }
+
+    open class Circle(_name: String) : Shape(_name) {
+        private var radius = 0.0;
+
+        //overriding getArea() method for Circle class
+        override fun getArea() {
+            println("Area is " + Math.PI * radius * radius)
+        }
+
+        // overriding setDimensions() method for Circle class
+        override fun setDimensions() {
+            println("Enter Radius of Circle : ")
+            radius = readLine()?.toDouble() ?: 0.0
+        }
+
+        // overriding printDimensions() method for Circle class
+        override fun printDimensions() {
+            println("Radius is : $radius")
+        }
+    }
+
+
+    open class Triangle(_name: String) : Shape(_name) {
+        // declaring variables as 'protected' for Triangle class
+        //so that EquilateralTriangle class can access them directly
+        protected var a = 0.0;
+        protected var b = 0.0;
+        protected var c = 0.0;
+
+        //overriding getArea() method for Triangle class
+        override fun getArea() {
+            val s = (a + b + c) / 2.0;
+            println("Area is " + Math.sqrt(s * (s - a) * (s - b) * (s - c)))
+        }
+
+        // overriding setDimensions() method for Triangle class
+        override fun setDimensions() {
+            println("Enter First side of Triangle : ")
+            a = readLine()?.toDouble() ?: 0.0
+            println("Enter Second side of Triangle : ")
+            b = readLine()?.toDouble() ?: 0.0
+            println("Enter Third side of Triangle : ")
+            c = readLine()?.toDouble() ?: 0.0
+        }
+
+        // overriding printDimensions() method for Triangle class
+        override fun printDimensions() {
+            //Earlier the print statement for EquilateralTriangle class was used.
+            println("The First Side of Triangle is : $a")
+            println("The Second Side of Triangle is : $b")
+            println("The Third Side of Triangle is : $c")
+        }
+
+    }
+
+    // Implemented EquilateralTriangle class, inherited from Triangle class
+    open class EquilateralTriangle(_name: String) : Triangle(_name) {
+
+        //overriding setDimensions method
+        override fun setDimensions() {
+            //asking and reading only one side's value
+            println("Enter Side of Equilateral Triangle : ")
+            a = readLine()?.toDouble() ?: 0.0
+            //assigning value of a to b and c
+            b = a
+            c = a
+        }
+
+        //overriding printDimensions method
+        override fun printDimensions() {
+            println("The Side of Equilateral triangle is : $a")
+        }
+
+    }
+}
+
+
+
